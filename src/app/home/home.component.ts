6b256a6066c8ab111bc60dbc0eaecb4a0029ccc3
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-05 12:12:19 +1000

    RxJs 6 without rxjs-compat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
-import { Observer } from 'rxjs/Observer';
-import { Subscription } from 'rxjs/Subscription';
+import {map} from 'rxjs/operators';
+import { Observable,Observer,Subscription,interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -21,11 +19,11 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    const myNumber = Observable.interval(1000)
-    .map(
+    const myNumber = interval(1000)
+    .pipe(map(
       (data:number)=>{
         return data*2;
-    });
+    }));
     this.numbersOnSubscription = myNumber.subscribe(
       (number: number) => {
         console.log(number);
