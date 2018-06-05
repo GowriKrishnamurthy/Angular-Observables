84010b9dcb0f1581c8e13edb77e204a03087fa15
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-06-05 11:53:16 +1000

    Using Subjects to pass and listen to data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import {  Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 import { Observer } from 'rxjs/Observer';
 import { Subscription } from 'rxjs/Subscription';
@@ -9,12 +9,12 @@ import { Subscription } from 'rxjs/Subscription';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit,OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  numbersOnSubscription:Subscription;
-  customOnSubscription:Subscription;
-  
-  ngOnDestroy(){
+  numbersOnSubscription: Subscription;
+  customOnSubscription: Subscription;
+
+  ngOnDestroy() {
     this.customOnSubscription.unsubscribe();
     this.numbersOnSubscription.unsubscribe();
   }
@@ -22,16 +22,15 @@ export class HomeComponent implements OnInit,OnDestroy {
 
   ngOnInit() {
 
-    const myNumber=Observable.interval(1000);
+    const myNumber = Observable.interval(1000);
     this.numbersOnSubscription = myNumber.subscribe(
-      (number:number)=>{
+      (number: number) => {
         console.log(number);
       }
     );
 
-    const myObservable=Observable.create(
-      (observer:Observer<string>)=>
-      {
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
         setTimeout(() => {
           observer.next('first package')
         }, 2000);
@@ -45,10 +44,10 @@ export class HomeComponent implements OnInit,OnDestroy {
           observer.error('this does not work');
         }, 5000);
       });
-   this.customOnSubscription= myObservable.subscribe(
-     (data:string)=> { console.log(data); },
-     (error:string)=> { console.log(error); },
-     ()=> { console.log("completed"); }
-   );   
+    this.customOnSubscription = myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log("completed"); }
+    );
   }
 }
